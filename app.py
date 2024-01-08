4c8929362983b47c804c29a10f2d6051df52d748
Ruby Lu <rubylu12345@gmail.com>
2024-01-08 16:52:12 -0500

    set up flask application

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route('/analyze_sentiment', methods=['POST'])
+def analyze_sentiment():
+    data = request.get_json()
+    comment_text = data.get('comment')
+    is_negative = True  # placeholder
+    return jsonify({'is_negative': is_negative})
+
+if __name__ == '__main__':
+    app.run(debug=True)
