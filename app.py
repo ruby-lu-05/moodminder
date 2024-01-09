57d37aca1ecb3f93008f941b75ed150214dea738
sbrina-w <sabrinawang93@gmail.com>
2024-01-09 14:58:51 -0500

    added chrome extension files

--- app.py
+++ app.py
@@ -15,5 +15,25 @@ def analyze_sentiment():
 
     return jsonify({'is_negative': is_negative})
 
+# @app.route('/test', methods=['GET'])
+# def test():
+#     comment_text = "I feel very neutral about this video"
+
+#     payload = {'comment': comment_text}
+#     response = app.test_client().post('/analyze_sentiment', json=payload, content_type = 'application/json')
+
+#     if response.status_code == 200:
+#         data = response.json
+#         is_negative = data['is_negative']
+#         if is_negative:
+#             return "negative."
+#         else:
+#             return "not negative."
+#     else:
+#         return f"Error: {response.status_code}"
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
