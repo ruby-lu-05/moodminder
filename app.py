c4143bb9ed51be5a08bf6169ade88dd9898cb8b4
sbrina-w <sabrinawang93@gmail.com>
2024-01-09 22:33:01 -0500

    changed sentiment analysis to vader as it works better with social media speech, updated extension permissions for reddit instead of Youtube

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 
@@ -7,39 +7,19 @@ app = Flask(__name__)
 def analyze_sentiment():
     data = request.json
     comments = data.get('comments', [])
+    sentiment = SentimentIntensityAnalyzer()
 
-    # perform sentiment analysis
+    # perform sentiment analysis 
     analysis_results = []
     for comment in comments:
-        analysis = TextBlob(comment)
-        polarity = analysis.sentiment.polarity
-        is_negative = polarity < 0
+        analysis = sentiment.polarity_scores(comment)
+        compound = analysis.get('compound')
+        # from scoring system of vader, compound score of less than -0.05 is deemed 'negative'
+        is_negative = (compound <= -0.05)
+
         analysis_results.append({'comment': comment, 'is_negative': is_negative})
 
     return jsonify({'sentiment_analysis': analysis_results})
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-# TESTING FOR INITIAL FLASK SERVER AND SENTIMENT ANALYSIS
-# @app.route('/test', methods=['GET'])
-# def test():
-#     comment_text = "I love this video. It's amazing!"
-
-#     payload = {'comment': comment_text}
-#     response = app.test_client().post('/analyze_sentiment', json=payload, content_type = 'application/json')
-
-#     if response.status_code == 200:
-#         data = response.json
-#         is_negative = data['is_negative']
-#         if is_negative:
-#             return "negative."
-#         else:
-#             return "not negative."
-#     else:
-#         return f"Error: {response.status_code}"
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
