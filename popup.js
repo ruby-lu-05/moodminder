d9338b88b82c26d866c8c7fef885636c9e6140ed
Ruby Lu <rubylu12345@gmail.com>
2024-01-11 20:39:37 -0500

    styled the extension popup

--- popup.js
+++ popup.js
@@ -1,25 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.getElementById('toggleButton');
+  const toggleButton = document.getElementById('switch');
+  const blurButton = document.getElementById('blur-button');
+  const hideButton = document.getElementById('hide-button');
   let extensionState = false;
 
   // Retrieve extension state from Chrome storage
   chrome.storage.local.get('extensionState', function (data) {
-      extensionState = data.extensionState || false;
-      updateToggleButton(extensionState);
+    extensionState = data.extensionState || false;
+    updateToggleButton(extensionState);
   });
 
-  toggleButton.addEventListener('click', function () {
-      extensionState = !extensionState;
-      updateToggleButton(extensionState);
+  toggleButton.addEventListener('change', function () {
+    extensionState = toggleButton.checked;
+    updateToggleButton(extensionState);
 
-      // Save the updated extension state to Chrome storage
-      chrome.storage.local.set({ 'extensionState': extensionState });
+    // Save the updated extension state to Chrome storage
+    chrome.storage.local.set({ 'extensionState': extensionState });
 
-      // Send a message to background.js to update the state
-      chrome.runtime.sendMessage({ extensionState: extensionState });
+    // Send a message to background.js to update the state
+    chrome.runtime.sendMessage({ extensionState: extensionState });
+  });
+
+  blurButton.addEventListener('click', function () {
+    blurButton.classList.add('active');
+    hideButton.classList.remove('active');
+  });
+
+  hideButton.addEventListener('click', function () {
+    hideButton.classList.add('active');
+    blurButton.classList.remove('active');
   });
 
   function updateToggleButton(state) {
-      toggleButton.innerText = state ? 'OFF' : 'ON';
+    toggleButton.checked = state;
   }
 });
